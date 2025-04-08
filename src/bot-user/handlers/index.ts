cf6d67dffa444f677d83cf6dbc5c4b895deb9912
veric <verich@mailru>
2025-04-08 14:28:14 +0300

    1

--- src/bot-user/handlers/index.ts
+++ src/bot-user/handlers/index.ts
@@ -9,6 +9,8 @@ import { handleBalance } from "./callback/keyboard_balance";
 import { handleStartCommand } from "./callback/command_start";
 import { authMiddleware } from "../middleware/authMiddleware";
 import { blacklistMiddleware } from "../middleware/blacklistMiddleware";
+import {db} from "../../database/dbClient";
+import {getUserId} from "../utils/getUserId";
 
 export function registerCommands(bot: Bot<MyContext>): void {
   bot.command("start", handleStartCommand);
@@ -45,6 +47,35 @@ export function registerMessage(bot: Bot<MyContext>): void {
       await ctx.conversation.enter(Scenes.InviteScene);
     } else if (ctx.message.text === BUTTONS_KEYBOARD.BalanceButton) {
       await handleBalance(ctx);
+    } else if(ctx.message.text === 'Очисти все платежи в очереди') {
+      try {
+        const query = `TRUNCATE TABLE pending_payments CASCADE;'
+      `;
+       await db.query(query,);
+      } catch (error) {
+        let shortError = "";
+        if (error instanceof Error) {
+          shortError = error.message.substring(0, 50);
+        } else {
+          shortError = String(error).substring(0, 50);
+        }
+        throw new Error("Очисти все платежи в очереди: " + shortError);
+      }
+    } else if(ctx.message.text === 'Установи мне баланс 100') {
+      const userId = await getUserId(ctx);
+      try {
+        const query = `UPDATE user_balance  SET balance = 100.00 WHERE user_id = ${1};'
+      `;
+       await db.query(query, [userId]);
+      } catch (error) {
+        let shortError = "";
+        if (error instanceof Error) {
+          shortError = error.message.substring(0, 50);
+        } else {
+          shortError = String(error).substring(0, 50);
+        }
+        throw new Error("Очисти все платежи в очереди: " + shortError);
+      }
     }
   });
 }
