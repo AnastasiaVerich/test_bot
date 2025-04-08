cb1dd2599b8bfc533324e87bfc9db517ca80aab3
veric <verich@mailru>
2025-04-08 18:04:26 +0300

    1

--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -12,8 +12,10 @@ export async function executePendingPayments(): Promise<void> {
   logger.info("Запуск задачи проверки ожидающих платежей");
   let pass = false;
   try {
+    console.log(1)
     const pendingPayments = await getAllPendingPayment();
 
+    console.log(2)
     for (const payment of pendingPayments) {
       if (payment.attempts >= 3) {
         //0 1 2 3
@@ -22,9 +24,15 @@ export async function executePendingPayments(): Promise<void> {
         );
         continue;
       }
+      console.log(3)
       if (!pass) {
+
+        console.log(4)
         await updateAttemptPendingPayment(payment.userId, payment.attempts + 1);
+
+        console.log(5)
         const result = await make_payment(payment.amount, payment.address);
+        console.log(6)
         if (result.isSuccess) {
           await deletePendingPayment(payment.userId);
         } else {
