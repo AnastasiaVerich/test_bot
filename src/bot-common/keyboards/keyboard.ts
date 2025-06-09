68fde58c31573b599c3719343ac099d0bc7ba92d
veric <verich@mailru>
2025-06-09 17:32:40 +0300

    1

--- src/bot-common/keyboards/keyboard.ts
+++ src/bot-common/keyboards/keyboard.ts
@@ -102,7 +102,7 @@ export const WebAppKeyboard = (
           userPhone,
           type: type,
           isSavePhoto: isSavePhoto,
-          version: 6,
+          version: 7,
         }),
       )}`,
     )
